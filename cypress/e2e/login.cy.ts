9bf561f082c03ed25ca4fe0bbf17b026623a69ae
lifi8888 <liftes08@gmail.com>
2025-08-04 13:09:18 +0200

    test: add Bejelentkezés oldal teszt

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,9 +1,35 @@
 describe('Bejelentkezés', () => {
-  it('megnyitja a login oldalt és megjelennek az elemek', () => {
+  beforeEach(() => {
     cy.visit('/login');
+  });
+
+  it('megnyitja a login oldalt és megjelennek az elemek', () => {
     cy.get('h1').should('contain', 'Bejelentkezés');
+
     cy.get('input[name="email"]').should('exist');
     cy.get('input[name="password"]').should('exist');
     cy.get('button[type="submit"]').should('contain', 'Bejelentkezés');
+  });
+
+  it('a fejléc 32px betűméretű és sötétkék színű', () => {
+    cy.get('h1')
+      .should('exist')
+      .and('have.css', 'font-size', '32px')
+      .and('have.css', 'color', 'rgb(0, 0, 139)'); // sötétkék szín
+  });
+
+  it('az oldal tartalma középre van igazítva', () => {
+    cy.get('#login-container')
+      .should('exist')
+      .and('have.css', 'display', 'flex')
+      .and('have.css', 'justify-content', 'center')
+      .and('have.css', 'align-items', 'center');
+  });
+
+  it('a login doboz nem szélesebb mint 400px', () => {
+    cy.get('#login-box')
+      .should('exist')
+      .invoke('outerWidth')
+      .should('be.lte', 400);
   });
 });
